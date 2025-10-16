2f39816ae1a93675f625ed0326f78b2d2306022b
rjsquire <rjsquire@gmail.com>
2025-10-15 20:38:09 -0400

    Initial commit.  duckdb-go simple example

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"log"
+
+	_ "github.com/duckdb/duckdb-go/v2"
+)
+
+func main() {
+	db, err := sql.Open("duckdb", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec(`CREATE TABLE people (id INTEGER, name VARCHAR)`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = db.Exec(`INSERT INTO people values (42, 'John')`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var (
+		id   int
+		name string
+	)
+	row := db.QueryRow(`SELECT id, name FROM people`)
+	err = row.Scan(&id, &name)
+	if errors.Is(err, sql.ErrNoRows) {
+		log.Println("no rows")
+	} else if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("id: %d, name: %s\n", id, name)
+
+}
